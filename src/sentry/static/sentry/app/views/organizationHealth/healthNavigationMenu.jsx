82fec8273fa29cb1f03b41840e7954be689da956
Billy Vong <billy@sentry.io>
2018-07-30 16:51:25 -0700

    wip lots of stuff make filler bar not have hover effect

--- src/sentry/static/sentry/app/views/organizationHealth/healthNavigationMenu.jsx
+++ src/sentry/static/sentry/app/views/organizationHealth/healthNavigationMenu.jsx
@@ -23,7 +23,9 @@ const HealthNavigationMenu = styled(
               <NavItem to={`/organizations/${organization.slug}/health/`}>
                 {t('Overview')}
               </NavItem>
-
+              <NavItem to={`/organizations/${organization.slug}/health/errors/`}>
+                {t('Errors')}
+              </NavItem>
               <NavItem to={`/organizations/${organization.slug}/health/transactions/`}>
                 {t('Transactions')}
               </NavItem>
